aa3db4fb2fb257adf7347b2a9afd53753ab3007f
sisazac <sisazac23@gmail.com>
2023-05-16 17:14:24 +0000

    Added Queue data structure

---
+++ Exercises/Chapter3/Notes/Queue.py
@@ -0,0 +1,21 @@
+class Queue():
+    
+    def __init__(self) -> None:
+        '''Create a new queue'''
+        self._items = []
+
+    def is_empty(self):
+        '''Check if the queue is empty'''
+        return not bool(self._items)
+    
+    def enqueue(self,item):
+        '''Add a item to the queue'''
+        self._items.insert(0,item)
+
+    def dequeue(self):
+        '''Remove item of the queue'''
+        self._items.pop()
+    
+    def size(self):
+        '''Get the number of items in the queue'''
+        return len(self._items)
